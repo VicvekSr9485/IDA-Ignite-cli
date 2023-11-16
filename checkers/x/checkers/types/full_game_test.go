b2a239ffdde92424569409f2ee3bd95ff288c9f3
VicvekSr9485 <olamideoso65@gmail.com>
2023-11-16 04:22:26 +0100

    add message validation and handling

--- checkers/x/checkers/types/full_game_test.go
+++ checkers/x/checkers/types/full_game_test.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/alice/checkers/testutil"
 	"github.com/alice/checkers/x/checkers/rules"
-	"github.com/alice/checkers/x/checkers/testutil"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
@@ -27,22 +27,22 @@ func GetStoredGame1() types.StoredGame {
 }
 
 func TestCanGetAddressBlack(t *testing.T) {
-    aliceAddress, err1 := sdk.AccAddressFromBech32(alice)
-    black, err2 := GetStoredGame1().GetBlackAddress()
-    require.Equal(t, aliceAddress, black)
-    require.Nil(t, err2)
-    require.Nil(t, err1)
+	aliceAddress, err1 := sdk.AccAddressFromBech32(alice)
+	black, err2 := GetStoredGame1().GetBlackAddress()
+	require.Equal(t, aliceAddress, black)
+	require.Nil(t, err2)
+	require.Nil(t, err1)
 }
 
 func TestGetAddressWrongBlack(t *testing.T) {
-    storedGame := GetStoredGame1()
-    storedGame.Black = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d4" // Bad last digit
-    black, err := storedGame.GetBlackAddress()
-    require.Nil(t, black)
-    require.EqualError(t,
-        err,
-        "black address is invalid: cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d4: decoding bech32 failed: invalid checksum (expected 3xn9d3 got 3xn9d4)")
-    require.EqualError(t, storedGame.Validate(), err.Error())
+	storedGame := GetStoredGame1()
+	storedGame.Black = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d4" // Bad last digit
+	black, err := storedGame.GetBlackAddress()
+	require.Nil(t, black)
+	require.EqualError(t,
+		err,
+		"black address is invalid: cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d4: decoding bech32 failed: invalid checksum (expected 3xn9d3 got 3xn9d4)")
+	require.EqualError(t, storedGame.Validate(), err.Error())
 }
 
 func TestCanGetAddressRed(t *testing.T) {
